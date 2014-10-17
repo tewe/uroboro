c75de945dda814f6a961699fd98185c470354685
tewe <tewe@users.noreply.github.com>
2014-10-17 22:57:41 +0200

    Start working around ugly AST

--- src/Uroboro/Checker.hs
+++ src/Uroboro/Checker.hs
@@ -7,8 +7,18 @@ import Uroboro.Syntax
 
 type Context = [(Identifier, Type)]
 
+signature :: Library -> Identifier -> Maybe Signature
+signature ((FunctionDefinition s@(Signature f _ _) _):xs) f' = if f == f' then Just s else signature xs f'
+signature (x:xs) f = signature xs f
+signature _ _ = Nothing
+
 typecheck :: Library -> Context -> Exp -> Type -> Either String Exp
 typecheck p c e t = case e of
+
     Variable x -> case lookup x c of
         Just t' -> if t == t' then Right e else Left "mismatch"
+        Nothing -> Left "unknown"
+
+    Application f args -> case signature p f of
+        Just (Signature _ ts t') -> if t == t' then Right e else Left "mismatch"
         Nothing -> Left "unknown"
