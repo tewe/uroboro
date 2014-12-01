0e2f6a754e037f91926938d6b642a93c2ef377fd
tewe <tewe@users.noreply.github.com>
2014-12-01 13:55:45 +0100

    New AST nodes

--- src/Uroboro/Tree.hs
+++ src/Uroboro/Tree.hs
@@ -2,6 +2,9 @@ module Uroboro.Tree where
 
 type Identifier = String
 type Type = Identifier
+
+{- Parse Tree -}
+
 data PExp = PVar Identifier
           | PApp Identifier [PExp]
           | PDes Identifier [PExp] PExp deriving (Eq, Show)
@@ -21,3 +24,16 @@ data PTRule = PTRule PQ PExp deriving (Eq, Show)
 data PT = PTPos Type [PTCon]
         | PTNeg Type [PTDes]
         | PTFun Identifier [Type] Type [PTRule] deriving (Eq, Show)
+
+{- Typed Syntax Tree -}
+
+data TExp = TVar Type Identifier
+          | TApp Type Identifier [TExp]
+          | TCon Type Identifier [TExp]
+          | TDes Type Identifier [TExp] TExp deriving (Show, Eq)
+
+data TP = TPVar Type Identifier
+        | TPCon Type Identifier [TP] deriving (Show, Eq)
+
+data TQ = TQApp Type Identifier [TP]
+        | TQDes Type Identifier [TP] TQ deriving (Show, Eq)
