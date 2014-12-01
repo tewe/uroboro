39868f7dc2758daa0a965d4ed146c1a7b133288a
tewe <tewe@users.noreply.github.com>
2014-12-01 14:59:54 +0100

    Rewrite type checker

--- src/Uroboro/Checker.hs
+++ src/Uroboro/Checker.hs
@@ -1,164 +1,21 @@
-module Uroboro.Checker
-    (
-      check
-    , checkp
-    , checkc
-    , checkr
-    , typecheck
-    , etype
-    ) where
+module Uroboro.Checker where
 
-import Control.Monad (mapM, zipWithM, foldM)
-import Data.Either (isRight)
 import Data.List (find)
-import Data.Maybe (listToMaybe)
 
-import Uroboro.Syntax
-import Uroboro.Abstract
+import Uroboro.Tree
 
-etype :: TExp -> Type
-etype (TVar _ t) = t
-etype (TApp _ _ t) = t
-etype (TCon _ _ t) = t
-etype (TDes _ _ _ t) = t
+-- |For foldlM
+pos :: [PT] -> PT -> Either String [PT]
+pos defs def@(PTPos t constructors)
+    | find (mismatch t) constructors /= Nothing = Left $
+        "Definition Mismatch: " ++ t ++ " is not returned by all of its constructors"
+    | find (existing t) defs /= Nothing         = Left $
+        "Shadowed Definition: " ++ t ++ " is defined more than once"
+    | otherwise                                 = Right (def:defs)
+  where
+    existing name (PTPos n _) = name == n
+    existing name (PTNeg n _) = name == n
+    existing _ _ = False
 
-qtype :: TQ -> Type
-qtype (TQApp _ _ t) = t
-qtype (TQDes _ _ _ t) = t
-
-type Context = [(Identifier, Type)]
-
-pcontext :: TP -> Context
-pcontext (TPVar x t) = [(x, t)]
-pcontext (TPCons _ tps _) = concat $ map pcontext tps
-
-qcontext :: TQ -> Context
-qcontext (TQApp _ tps _) = concat $ map pcontext tps
-qcontext (TQDes tq _ tps _) = concat [qcontext tq, concat $ map pcontext tps]
-
--- |Check definitions.
-typecheck :: Library -> Definition -> Either String TRules
-typecheck p (FunctionDefinition s rs) = mapM (checkr p s) rs
-typecheck p (DataDefinition n _) = return [] -- TODO check for name clash
-typecheck p (CodataDefinition n _) = return []
-
--- |Check typing of rule for given function.
-checkr :: Library -> Signature -> Rule -> Either String (TQ, TExp)
-checkr p s@(Signature _ _ t) (Rule l r) = do
-    tq <- checkc p s l
-    te <- check p (qcontext tq) r t
-    return (tq, te)
-
--- |Check that copattern eliminates given type into inferred type, yielding context.
-checkc :: Library -> Signature -> Copattern -> Either String TQ
-checkc l (Signature f ts t) (Hole ps) | length ts == length ps = do
-    cs <- zipWithM (checkp l) ps ts
-    return $ TQApp f cs t
-                                      | otherwise = Left "wrong number of arguments"
-checkc l s (DestructorCopattern p n ps) = do
-    tq <- checkc l s p
-    (ts, rt) <- nu l (qtype tq) n
-    cs <- zipWithM (checkp l) ps ts
-    if length ps /= length ts then Left "wrong number of arguments" else return $ TQDes tq n cs rt
-
-insert :: Context -> (Identifier, Type) -> Either String Context
-insert c (k, v) = maybe (Right ((k, v):c)) (\v' -> Left "duplicate") (lookup k c)
-
-union :: Context -> Context -> Either String Context
-union a b = foldM insert a b
-
--- |Typecheck patterns
-checkp :: Library -> Pattern -> Type -> Either String TP
-checkp _ (VariablePattern x) t = return $ TPVar x t
-checkp p (ConstructorPattern c ps) t = do
-    ts <- constructor p t c
-    cs <- zipWithM (checkp p) ps ts
-    if length ps /= length ts then Left "wrong number of arguments" else return $ TPCons c cs t
-
-signature :: [Signature] -> Identifier -> Either String [Type]
-signature ((Signature n ts _):_) n' | n == n' = return ts
-signature (_:ss) n = signature ss n
-signature _ _ = Left "unknown"
-
-sigma :: Library -> Identifier -> Either String ([Type], Type)
-sigma ((FunctionDefinition (Signature n' ts t) _):_) n | n' == n = return (ts, t)
-sigma (_:xs) n = sigma xs n
-sigma _ _ = Left "unknown"
-
-constructor :: Library -> Type -> Identifier -> Either String [Type]
-constructor ((DataDefinition d' ss):_) d n | d' == d = signature ss n
-constructor (_:xs) d n = constructor xs d n
-constructor _ _ _ = Left "unknown"
-
-checkargs :: Library -> Context -> [Exp] -> [Type] -> Either String [TExp]
-checkargs _ _ [] [] = return []
-checkargs p c (e:es) (t:ts) = do
-    te <- check p c e t
-    tes <- checkargs p c es ts
-    return (te:tes)
-checkargs _ _ _ _ = Left "wrong number of arguments"
-
-infers :: [Signature] -> Identifier -> [Type] -> Either String Type
-infers ((Signature n ts t):_) n' ts' | n == n' && ts == ts' = return t
-infers (_:ss) n ts = infers ss n ts
-infers _ _ _ = Left "unknown"
-
-mu :: Library -> Identifier -> [Type] -> Either String Type
-mu ((DataDefinition _ ss):_) c ts = infers ss c ts
-mu (_:ds) c ts = mu ds c ts
-mu _ _ _ = Left "unknown"
-
-inferd :: [Signature] -> Identifier -> Either String ([Type], Type)
-inferd ((Signature n ts t):_) n' | n == n' = return (ts, t)
-inferd (_:ss) n = inferd ss n
-inferd _ _ = Left "unknown"
-
--- |Look up signature of named destructor for type.
-nu :: Library -> Type -> Identifier -> Either String ([Type], Type)
-nu ((CodataDefinition c' ss):_) c d | c' == c = inferd ss d
-nu (_:ds) c d = nu ds c d
-nu _ _ _ = Left "unknown"
-
-inferc :: Library -> Context -> Identifier -> [Exp] -> Either String TExp
-inferc p c n es = do
-    tes <- mapM (infer p c) es
-    let ts = map etype tes
-    t <- mu p n ts
-    return $ TCon n tes t
-
-infer :: Library -> Context -> Exp -> Either String TExp
-infer _ c (Variable x) = maybe (Left "unknown") (Right . TVar x) $ lookup x c
-infer p c (Application n es) = case (infer p c (FunctionApplication n es), infer p c (ConstructorApplication n es)) of
-    (Right e, Left _) -> Right e
-    (Left _, Right e) -> Right e
-    _ -> Left "ambiguous"
-infer p c (FunctionApplication f es) = do
-    (ts, t) <- sigma p f
-    tes <- checkargs p c es ts
-    return $ TApp f tes t
-infer p c (ConstructorApplication n es) = inferc p c n es
-infer p c (DestructorApplication e n es) = do
-    te <- infer p c e
-    (ts, t) <- nu p (etype te) n
-    tes <- checkargs p c es ts
-    return $ TDes n te tes t
-
--- |Typecheck terms
-check :: Library -> Context -> Exp -> Type -> Either String TExp
-check p c e@(Variable x) t = do
-    te <- infer p c e
-    if etype te == t then return te else Left "mismatch"
-check p c (Application n es) t = case (check p c (FunctionApplication n es) t, check p c (ConstructorApplication n es) t) of
-    (Right e, Left _) -> Right e
-    (Left _, Right e) -> Right e
-    _ -> Left "ambiguous"
-check p c e@(FunctionApplication _ _) t = do
-    te <- infer p c e
-    if etype te == t then return te else Left "mismatch"
-check p c (ConstructorApplication x es) t = do
-    ts <- constructor p t x
-    tes <- checkargs p c es ts
-    return $ TCon x tes t
-check p c d@(DestructorApplication e n es) t = do
-    te <- infer p c d
-    if etype te == t then return te else Left "mismatch"
+    mismatch name (PTCon _ _ n) = name /= n
+pos _ _ = return []
--- test/Uroboro/CheckerSpec.hs
+++ test/Uroboro/CheckerSpec.hs
@@ -3,89 +3,30 @@ module Uroboro.CheckerSpec
       spec
     ) where
 
-import Data.Either (isLeft, isRight)
-
 import Test.Hspec
 
-import Paths_uroboro
-import Uroboro.Parser
-import Uroboro.Syntax
-import Uroboro.Abstract
+import Uroboro.Parser (parseDef)
 import Uroboro.Checker
-import Utils
+import Utils (parseString)
 
-prelude = getDataFileName "samples/prelude.uro" >>= parseFromFile library
-
--- |Context using prelude
-c = [
-      ("i", "Int")
-    , ("f", "IntToInt")
-    , ("g", "TwoIntToInt")
-    , ("l", "ListOfInt")
-    , ("s", "StreamOfInt")
-    ]
+-- |Assert error message
+shouldFail :: Show a => Either String a -> String -> Expectation
+Left msg `shouldFail` prefix = takeWhile (/= ':') msg `shouldBe` prefix
+Right  x `shouldFail` prefix = expectationFailure
+    ("expected: " ++ prefix ++ "\n but got: " ++ show x)
 
 spec :: Spec
 spec = do
-    describe "terms" $ do
-        context "when variables" $ do
-            it "may be unknown" $ do
-                check [] c (Variable "x") "Int" `shouldBe` Left "unknown"
-            it "may not match" $ do
-                check [] c (Variable "f") "Int" `shouldBe` Left "mismatch"
-            it "may check out" $ do
-                let e = Variable "i"
-                check [] c e "Int" `shouldBe` Right (TVar "i" "Int")
-        context "when constructors" $ do
-            it "may check out" $ do
-                p <- prelude
-                let e = ConstructorApplication "succ" [Variable "i"]
-                check p c e "Int" `shouldSatisfy` isRight
-            it "check the return type" $ do
-                p <- prelude
-                let e = ConstructorApplication "succ" [Variable "i"]
-                check p c e "T" `shouldBe` Left "unknown"
-            it "check the number of arguments" $ do
-                p <- prelude
-                let e = ConstructorApplication "succ" [Variable "i", Variable "s"]
-                check p c e "Int" `shouldBe` Left "wrong number of arguments"
-            it "recurse" $ do
-                p <- prelude
-                let e = ConstructorApplication "succ" [Variable "s"]
-                check p c e "Int" `shouldBe` Left "mismatch"
-            it "may not exist" $ do
-                p <- prelude
-                let e = ConstructorApplication "full" []
-                check p c e "ListOfInt" `shouldBe` Left "unknown"
-        context "when destructors" $ do
-            it "may check out" $ do
-                p <- prelude
-                e <- parseString expression "f.apply(i)"
-                check p c e "Int" `shouldBe` Right (TDes "apply" (TVar "f" "IntToInt") [TVar "i" "Int"] "Int")
-        context "when functions" $ do
-            it "may check out" $ do
-                p <- prelude
-                check p [("f", "IntToInt"), ("l", "ListOfInt")] (FunctionApplication "map" [Variable "f", Variable "l"]) "ListOfInt" `shouldBe` Right (TApp "map" [TVar "f" "IntToInt", TVar "l" "ListOfInt"] "ListOfInt")
-        context "when applications" $ do
-            it "infer as functions" $ do
-                p <- prelude
-                e <- parseString expression "map(f, l)"
-                check p c e "ListOfInt" `shouldBe` Right (TApp "map" [TVar "f" "IntToInt", TVar "l" "ListOfInt"] "ListOfInt")
-            it "infer as constructors" $ do
-                p <- prelude
-                e <- parseString expression "succ(i)"
-                check p c e "Int" `shouldBe` Right (TCon "succ" [TVar "i" "Int"] "Int")
-    describe "patterns" $ do
-        it "bind variables" $ do
-            checkp [] (VariablePattern "x") "T" `shouldBe` Right (TPVar "x" "T")
-        it "nest" $ do
-            p <- prelude
-            e <- parseString pattern "succ(i)"
-            checkp p e "Int" `shouldBe` Right (TPCons "succ" [TPVar "i" "Int"] "Int")
-    describe "copatterns" $ do
-        it "can be holes" $ do
-            checkc [] (Signature "chr" ["Int"] "Char") (Hole [VariablePattern "i"]) `shouldBe` Right (TQApp "chr" [TPVar "i" "Int"] "Char")
-    describe "typecheck" $ do
-        it "checks functions" $ do
-            p <- prelude
-            typecheck p (last p) `shouldSatisfy` isRight -- TODO don't depend on order
+    describe "pos" $ do
+        it "checks return types" $ do
+            x:_ <- parseString parseDef "data Int where zero(): Float"
+            pos [] x `shouldFail` "Definition Mismatch"
+        it "prevents duplicates" $ do
+            x:xs <- parseString parseDef $ unlines
+                [ "data Int where zero(): Int"
+                , "data Int where succ(): Int"
+                ]
+            pos xs x `shouldFail` "Shadowed Definition"
+        it "folds" $ do
+            x:_ <- parseString parseDef "data Int where zero(): Int"
+            pos [] x `shouldBe` (Right [x])
