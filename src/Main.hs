846c61647bfd7bf705277b9c1bc4ee0933a8d528
tewe <tewe@users.noreply.github.com>
2014-12-19 02:11:13 +0100

    Parse command line options

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,30 @@
+import System.Console.GetOpt
 import System.Environment
 
 import Text.Parsec (parse)
 
-import Uroboro.Parser (parseDef)
+import Paths_uroboro
+import Uroboro.Checker (typecheck, inferPExp)
+import Uroboro.Interpreter (eval)
+import Uroboro.Parser (parseDef, parseExp)
+import Uroboro.Tree
 
-parseFromFile p fname = do
-    input <- readFile fname
-    return $ parse p fname input
+data Flag = SkipPrelude
+          | MainExpr String
+          | LoadLib FilePath deriving (Eq, Show)
+
+options :: [OptDescr Flag]
+options =
+    [ Option ['p'] [] (NoArg SkipPrelude) "do not include prelude"
+    , Option ['e'] [] (ReqArg MainExpr "EXPRESSION") "evaluate EXPRESSION"
+    ]
+
+mainOpts :: [String] -> IO [Flag]
+mainOpts argv = case getOpt Permute options argv of
+    (flags, paths, []) -> return $ flags ++ (map LoadLib paths)
+    (_, _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
+  where
+    header = "Usage: uroboro [OPTION...] files..."
 
 main :: IO ()
-main = do
-    args <- getArgs
-    lib <- parseFromFile parseDef (args !! 0)
-    case lib of
-        Left e -> print e
-        Right _ -> putStrLn $ "We can't type check yet, but your" ++
-            " code was recognized :)"
+main = getArgs >>= mainOpts >>= putStrLn . show
