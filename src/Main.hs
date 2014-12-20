c06e79566761963feb9ed082082a1c3ff8746865
tewe <tewe@users.noreply.github.com>
2014-12-20 14:46:02 +0100

    Change command line interface

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,29 @@
-import System.Console.GetOpt
-import System.Environment
+import Control.Exception (try)
+import Control.Monad (foldM)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
 
 import Text.Parsec (parse)
 
-import Paths_uroboro
 import Uroboro.Checker (typecheck, inferPExp)
 import Uroboro.Interpreter (eval)
 import Uroboro.Parser (parseDef, parseExp)
 import Uroboro.Tree
 
-data Flag = SkipPrelude
-          | MainExpr String
-          | LoadLib FilePath deriving (Eq, Show)
+data Mode = Help
+          | Typecheck [PT]
+          | Evaluate  [PT] PExp
 
-options :: [OptDescr Flag]
-options =
-    [ Option ['p'] [] (NoArg SkipPrelude) "do not include prelude"
-    , Option ['e'] [] (ReqArg MainExpr "EXPRESSION") "evaluate EXPRESSION"
-    ]
-
-mainOpts :: [String] -> IO [Flag]
-mainOpts argv = case getOpt (ReturnInOrder LoadLib) options argv of
-    (flags, [], []) -> return flags
-    (_, _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
-  where
-    header = "Usage: uroboro [OPTION...] files..."
+getOpt :: Mode -> String -> IO Mode
+getOpt _ _ = return Help
 
 main :: IO ()
-main = getArgs >>= mainOpts >>= putStrLn . show
+main = do
+    args <- getArgs
+    mode <- foldM getOpt Help args
+    case mode of
+        Help -> do
+            putStrLn "USAGE: uroboro files... [expression]"
+            exitFailure
+        Typecheck _ -> putStrLn "check"
+        Evaluate _ _ -> putStrLn "eval"
