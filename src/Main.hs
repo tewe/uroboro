9594e60b32744cab4da46520778265e56c438bf5
tewe <tewe@users.noreply.github.com>
2014-12-20 16:49:44 +0100

    Finish main executable

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,19 @@
-import Control.Exception (try)
-import Control.Monad (foldM)
+import Control.Monad (foldM, foldM_, zipWithM)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 
 import Text.Parsec (parse)
 
-import Uroboro.Checker (typecheck, inferPExp)
+import Uroboro.Checker (rules, emptyProgram, checkPT, inferPExp)
 import Uroboro.Interpreter (eval)
-import Uroboro.Parser (parseDef, parseExp)
+import Uroboro.Parser (Parser, parseDef, parseExp)
 import Uroboro.Tree
 
 data Mode = Help
           | Typecheck [FilePath]
           | Evaluate  [FilePath] String deriving (Eq, Show)
 
+-- |Parse command line options.
 getOpt :: [String] -> Mode
 getOpt args = case break (== "--") args of
     ([], _)     -> Help
@@ -21,12 +21,37 @@ getOpt args = case break (== "--") args of
     (x, [_, y]) -> Evaluate x y
     _           -> Help
 
+eitherIO :: Show a => Either a b -> IO b
+eitherIO (Left e)  = do
+    print e
+    exitFailure
+eitherIO (Right b) = return b
+
+-- |Switch monads.
+parseIO :: Parser a -> String -> String -> IO a
+parseIO parser fname input = eitherIO $ parse parser fname input
+
+-- |Load libraries.
+parseFiles :: [FilePath] -> IO [PT]
+parseFiles paths = do
+    lol <- mapM readFile paths >>= zipWithM (parseIO parseDef) paths
+    return $ concat lol
+
 main :: IO ()
 main = do
     args <- getArgs
     case getOpt args of
-        Evaluate _ _ -> putStrLn "eval"
-        Typecheck _ -> putStrLn "typecheck"
+        Evaluate paths input -> do
+            defs  <- parseFiles paths
+            pexp  <- eitherIO $ parse parseExp "command line" input
+
+            prog <- eitherIO $ foldM checkPT emptyProgram defs
+            texp  <- eitherIO $ inferPExp prog [] pexp
+
+            print $ eval (rules prog) texp
+        Typecheck paths -> do
+            defs  <- parseFiles paths
+            eitherIO $ foldM_ checkPT emptyProgram defs     -- Success is silent.
         Help -> do
             putStrLn "USAGE: uroboro FILES [-- EXPRESSION]"
             exitFailure
