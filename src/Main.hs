5d3ff76798749a9da8326eaeac4929146f9606b4
tewe <tewe@users.noreply.github.com>
2014-12-20 15:35:21 +0100

    Change and test command line options

--- src/Main.hs
+++ src/Main.hs
@@ -11,19 +11,22 @@ import Uroboro.Parser (parseDef, parseExp)
 import Uroboro.Tree
 
 data Mode = Help
-          | Typecheck [PT]
-          | Evaluate  [PT] PExp
+          | Typecheck [FilePath]
+          | Evaluate  [FilePath] String deriving (Eq, Show)
 
-getOpt :: Mode -> String -> IO Mode
-getOpt _ _ = return Help
+getOpt :: [String] -> Mode
+getOpt args = case break (== "--") args of
+    ([], _)     -> Help
+    (x, [])     -> Typecheck x
+    (x, [_, y]) -> Evaluate x y
+    _           -> Help
 
 main :: IO ()
 main = do
     args <- getArgs
-    mode <- foldM getOpt Help args
-    case mode of
-        Help -> do
-            putStrLn "USAGE: uroboro files... [expression]"
-            exitFailure
-        Typecheck _ -> putStrLn "check"
+    case getOpt args of
         Evaluate _ _ -> putStrLn "eval"
+        Typecheck _ -> putStrLn "typecheck"
+        Help -> do
+            putStrLn "USAGE: uroboro FILES [-- EXPRESSION]"
+            exitFailure
---
+++ test/MainSpec.hs
@@ -0,0 +1,26 @@
+module Uroboro.CheckerSpec
+    (
+      spec
+    ) where
+
+import Test.Hspec
+
+import Main
+
+spec :: Spec
+spec = do
+    describe "getOpt" $ do
+        it "accepts one path" $ do
+            let p = ["foo"]
+            getOpt p `shouldBe` Typecheck p
+        it "accepts two paths" $ do
+            let p = ["foo", "bar"]
+            getOpt p `shouldBe` Typecheck p
+        it "accepts expression" $ do
+            getOpt ["foo", "--", "bar"] `shouldBe` Evaluate ["foo"] "bar"
+        it "rejects expressions" $ do
+            getOpt ["foo", "--", "bar", "x"] `shouldBe` Help
+        it "requires paths" $ do
+            getOpt ["--", "bar"] `shouldBe` Help
+        it "is unforgiving" $ do
+            getOpt ["foo", "--"] `shouldBe` Help
